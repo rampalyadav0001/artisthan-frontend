f8f990aa1872c2a729c7141554758d79d2b2ce07
itSarthak <sarthakt3006@gmail.com>
2024-12-03 02:24:41 +0530

    Added Onboarding Screen

--- constants/GlobalHooks.tsx
+++
@@ -1,3 +0,0 @@
-import { useState } from "react";
-
-export const [isLoggedIn, setIsLoggedIn] = useState(false);
--- theme.ts
+++ theme.ts
@@ -1,4 +1,10 @@
 export const theme = {
   colorGreen: "#29b365",
   colorWhite: "#fff",
+  colors: {
+    backgroundColor: "#fff1c2",
+    backgroundHighlightColor: "#E60B15",
+    textColor: "#1b1b1b",
+    textHighlightColor: "#f0f0f0",
+  },
 };
