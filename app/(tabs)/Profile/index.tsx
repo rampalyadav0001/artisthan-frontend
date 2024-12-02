ad58d2b8d1af7764dc986d4b50ccc3d2af74f8cc
itSarthak <sarthakt3006@gmail.com>
2024-12-02 13:40:47 +0530

    Issue not resolved

--- app/(tabs)/Profile/index.tsx
+++ app/(tabs)/Profile/index.tsx
@@ -1,8 +1,11 @@
-import { StyleSheet } from "react-native";
+import { Button, StyleSheet, TouchableOpacity } from "react-native";
+
 import React from "react";
 import { Text, View } from "@/components/Themed";
+import { useRouter } from "expo-router";
 
 export default function index() {
+  const router = useRouter();
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Profile page</Text>
@@ -11,6 +14,11 @@ export default function index() {
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
+      <TouchableOpacity onPress={() => router.navigate("/Login")}>
+        <Text style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}>
+          + Log Out +
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
